1c00879c77f98e596bc9f14dce5094386d3e0caf
Aishat Abdulsalam <aishatabdulsalam76@gmail.com>
2025-12-27 15:07:27 +0100

    Update Chapter 1.hs

--- Practical task /Chapter 1.hs
+++ Practical task /Chapter 1.hs
@@ -16,3 +16,18 @@ increment x = x + 1
 main :: IO ()
 main = do
  print $ increment 5 
+
+--doubleThenIncrement: Uses function composition to apply double first and then increment.
+
+ double :: Int -> Int
+double x = x * 2
+
+increment :: Int -> Int
+increment x = x + 1
+
+doubleThenincrement :: Int -> Int
+doubleThenincrement = increment . double 
+
+main :: IO ()
+main = do
+ print $ doubleThenincrement 6
